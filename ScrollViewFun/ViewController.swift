b4eb21b1d292c26ce5aafb2033bb4c93f99aa409
github username <github email address>
2016-07-19 13:35:27 -0400

    image stackview with scrollview

--- ScrollViewFun/ViewController.swift
+++ ScrollViewFun/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagesWithEmotionsScroll: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //constraints
+        self.view.removeConstraints(self.view.constraints)
+        self.view.translatesAutoresizingMaskIntoConstraints = false
+        self.imagesWithEmotionsScroll.removeConstraints(self.imagesWithEmotionsScroll.constraints)
+        self.imagesWithEmotionsScroll.translatesAutoresizingMaskIntoConstraints = false
+        self.imagesWithEmotionsScroll.topAnchor.constraintEqualToAnchor(self.view.topAnchor, constant: 20).active = true
+        self.imagesWithEmotionsScroll.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor).active = true
+        self.imagesWithEmotionsScroll.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor).active = true
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
