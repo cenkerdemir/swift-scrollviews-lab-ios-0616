570df371f5822bdb441eb5a8f5dfa17400ebc54a
github username <cenkerdemir@yahoo.com>
2016-08-20 17:16:34 -0400

    dd

--- ScrollViewFun/ViewController.swift
+++ ScrollViewFun/ViewController.swift
@@ -24,8 +24,6 @@ class ViewController: UIViewController {
         self.imagesWithEmotionsScroll.topAnchor.constraintEqualToAnchor(self.view.topAnchor, constant: 20).active = true
         self.imagesWithEmotionsScroll.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor).active = true
         self.imagesWithEmotionsScroll.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor).active = true
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
