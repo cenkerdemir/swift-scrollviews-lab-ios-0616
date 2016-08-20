5960e2a020800b148f99bceb15f5c0ad39f200f4
github username <cenkerdemir@yahoo.com>
2016-08-20 17:36:45 -0400

    final

--- ScrollViewFun/ViewController.swift
+++ ScrollViewFun/ViewController.swift
@@ -15,15 +15,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        //constraints
-        self.view.removeConstraints(self.view.constraints)
-        self.view.translatesAutoresizingMaskIntoConstraints = false
-        self.imagesWithEmotionsScroll.removeConstraints(self.imagesWithEmotionsScroll.constraints)
-        self.imagesWithEmotionsScroll.translatesAutoresizingMaskIntoConstraints = false
-        self.imagesWithEmotionsScroll.topAnchor.constraintEqualToAnchor(self.view.topAnchor, constant: 20).active = true
-        self.imagesWithEmotionsScroll.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor).active = true
-        self.imagesWithEmotionsScroll.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor).active = true
     }
 
     override func didReceiveMemoryWarning() {
